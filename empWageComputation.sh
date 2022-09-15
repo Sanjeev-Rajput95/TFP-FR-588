38f670a1ee527a5f6a29845741ec7e29b9a62171
sanjeev <sanjeevsingh.77d@gmail.com>
2022-09-16 00:11:25 +0530

    ADDED:Calculate Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,11 +4,23 @@ echo "Welcome to Employee Wage computation Program"
 
 isPresent=0;
 isAbsent=1;
-checkAttence=$((RANDOM%2))
+perHrWage=20;
+hrsInDay=8;
 
-if [ $checkAttence -eq 0 ]
+checkAttendance=$((RANDOM%2))
+
+if [ $checkAttendance -eq 0 ]
 then
 	echo "Employee is Present";
 else
 	echo "Employee is Absent";
+fi
+
+if [ $checkAttendance -eq 0 ]
+then
+	PerDaySalary=$(( perHrWage * hrsInDay ))
+	echo "$PerDaySalary"
+else
+	PerDaySalary=0
+	echo "$PerDaySalary"
 fi
