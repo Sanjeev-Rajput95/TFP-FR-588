f238dfa4ac7937e9ae7a08bf76ca6be7d8a15fe8
sanjeev <sanjeevsingh.77d@gmail.com>
2022-09-19 00:54:01 +0530

    Store the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,6 +5,7 @@ isFullTIme=2;
 maxHrsInMonth=10;
 empRatePerHr=20;
 numWorkingDays=20;
+dailyWageArr[];
 
 totalEmpHrs=0;
 totalWorkingDays=0;
@@ -23,6 +24,13 @@ function getWorkingHours() {
 
 }
 
+function dailyWage() {
+        empHours=$1
+        dailyWage=$(( $empHours * $empRatePerHr ))
+        echo $dailyWage
+
+}
+
 
 while [[ $totalWorkHours -lt $maxHrsInMonth &&
 	$totalWorkingDays -lt $numWorkingDays ]]
@@ -30,6 +38,9 @@ do
 	((totalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3)) )"
 	totalWorkHours=$(($totalWorkHours+$workHours))
+	dailyWageArr[$totalWorkingDays]="$( dailyWage $workHours )"
 done
 
 totalSalary=$(( $totalWorkHours * $empRatePerHr ));
+
+echo "Daily Wage of an Employee is : ${dailyWageArr[@]} "
