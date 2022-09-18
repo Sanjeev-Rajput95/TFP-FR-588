e54c0b8e16bcd62975f878bb47b0245d97018d37
sanjeev <sanjeevsingh.77d@gmail.com>
2022-09-18 14:07:39 +0530

    Calculating WAges for a MOnth

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,23 @@
 #!/bin/bash -x
 
 isPartTime=1;
-isFullTime=2;
+isFullTIme=2;
+totalSalary=0;
 empRatePerHr=20;
-empCheck=$(( RANDOM%3 ));
+numWorkingDays=20;
 
-case $empCheck in
-	$isFullTime)
-		empHrs=12 ;;
-	$isPartTime)
-		empHrs=8 ;;
-	*)
-		empHrs=0 ;;
-esac
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$(( RANDOM%3 ));
+		case $empCheck in
+			$isFullTime)
+				empHrs=8 ;;
+			$isPartTime)
+				empHrs=4 ;;
+			*)
+				empHrs=8 ;;
+		esac
 
-Salary=$(( $empHrs*$empRatePerHr ));
+		Salary=$(( $empHrs*$empRatePerHr ));
+
+done
